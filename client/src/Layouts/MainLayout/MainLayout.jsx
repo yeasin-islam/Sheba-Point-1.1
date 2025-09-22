59ff612ed48dcf8b9ca53d0e220791a226b0dde9
shakil <shakil.sarkar1932@gmail.com>
2025-09-22 23:38:30 +0600

    Refactor code structure for improved readability and maintainability

--- client/src/Layouts/MainLayout/MainLayout.jsx
+++ client/src/Layouts/MainLayout/MainLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from '../../Components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import { Outlet } from 'react-router';
 import AIAssistant from './../../components/AIAssistant/AIAssistant';
