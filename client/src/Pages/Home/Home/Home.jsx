085b473ca65dde1572500e8a2d364ee7b6fc150c
Bokul98 <bokulsorkar96@gmail.com>
2025-09-27 16:45:14 +0600

    added section ServicesSections

--- client/src/Pages/Home/Home/Home.jsx
+++ client/src/Pages/Home/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import TopRatedDoctors from '../TopRatedDoctors/TopRatedDoctors';
 import OfferSection from '../OfferSection/OfferSection';
+import ServicesSection from '../Services/ServicesSection';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <ServicesSection />
             <TopRatedDoctors />
             <OfferSection />
         </div>
